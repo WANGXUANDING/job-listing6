9cf31c9b2ca932d4b9b2f04adff82c1b6799da0b
WANGXUANDING <wangxuanding1@gmail.com>
2018-05-28 23:55:14 +0200

    admin resume backup step8

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   resources :jobs do
     resources :resumes
-  end 
+  end
   namespace :admin do
     resources :jobs do
       member do
         post :publish
         post :hide
       end
+      resources :resumes
     end
   end
   root 'jobs#index'
